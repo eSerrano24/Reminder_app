5317d6717d5fece05be607942e952a06a544cbd9
Eric <eric@Eric.local>
2020-03-11 13:01:37 -0700

    added submit button and initial state to project.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: "Charlie",
-        job: "janitor"
-      },
-      {
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        name: "Dee",
-        job: "Aspiring Actress"
-      },
-      {
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
+    characters: []
   };
+
   removeCharacter = index => {
     const { characters } = this.state;
 
@@ -32,15 +17,22 @@ class App extends Component {
     });
   };
 
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
   render() {
     const { characters } = this.state;
 
     return (
       <div className="container">
+        <p>Add a character with a name and a job to the table.</p>
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
+        <h3>Add New</h3>
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
