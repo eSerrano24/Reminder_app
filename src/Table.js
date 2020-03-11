5afe74ab41cd7daa4988bd11099d8999cdea7e75
Eric <eric@Eric.local>
2020-03-10 20:02:06 -0700

    created states for characters, added delete button

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,19 @@
 import React, { Component } from "react";
 
+const Table = props => {
+  const { characterData, removeCharacter } = props;
+
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={characterData}
+        removeCharacter={removeCharacter}
+      />
+    </table>
+  );
+};
+
 let TableHeader = () => {
   return (
     <thead>
@@ -17,6 +31,9 @@ let TableBody = props => {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
@@ -24,15 +41,4 @@ let TableBody = props => {
   return <tbody>{rows}</tbody>;
 };
 
-class Table extends Component {
-  render() {
-    const { characterData } = this.props;
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={characterData} />
-      </table>
-    );
-  }
-}
 export default Table;
